929840d262ffdf44dbbdb6d8bb562c9c333629ce
Nours06 <88280654+Nours06@users.noreply.github.com>
2022-06-15 10:30:59 +0200

    Add password encryption
    Install bcrypt.
    Add hash function in utils and use it in user controller.

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { hashPassword } from 'src/utils/encryption.util';
 import { getUserByEmail, createUser } from '../services/user.service';
 
 export async function signup(req: Request, res: Response) {
@@ -16,14 +17,15 @@ export async function signup(req: Request, res: Response) {
   }
 
   return getUserByEmail(email)
-    .then((user) => {
+    .then(async (user) => {
       if (user) {
         res.send({ message: '⚠ User already exists!' });
       } else {
         if (passwordToConfirm !== confirmedPassword) {
           res.send({ messsage: "⚠ Passwords don't match!" });
         }
-        createUser(firstname, lastname, email, confirmedPassword)
+        const hashedPassword = await hashPassword(confirmedPassword);
+        await createUser(firstname, lastname, email, hashedPassword)
           .save()
           .then((createdUser) => {
             res.status(201).json(createdUser);
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,11 +1,5 @@
 import { create, findByEmail } from '../repositories/user.repository';
-import bcrypt from 'bcrypt';
 import { IUser } from 'src/models/User';
-
-async function hashPassword(password: string) {
-  const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
-}
 
 export function createUser(
   firstname: string,
